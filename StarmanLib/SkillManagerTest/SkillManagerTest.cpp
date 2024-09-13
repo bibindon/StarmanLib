525763e258e100682c48bd0630cd8af8113a1851
bibindon0814 <bibindon0814@gmail.com>
2024-09-13 13:11:52 +0900

    .

---
+++ StarmanLib/SkillManagerTest/SkillManagerTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SkillManagerTest
+{
+	TEST_CLASS(SkillManagerTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
