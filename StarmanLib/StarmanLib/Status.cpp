d10ccfc4a55bc91f81365865ada4c16938bb0332
bibindon0814 <bibindon0814@gmail.com>
2024-11-18 17:49:32 +0900

    .

--- StarmanLib/StarmanLib/Status.cpp
+++ StarmanLib/StarmanLib/Status.cpp
@@ -1,1 +1,321 @@
 #include "Status.h"
+
+Status* Status::obj { nullptr };
+
+Status* Status::GetObj()
+{
+    if (obj == nullptr)
+    {
+        obj = new Status();
+    }
+    return obj;
+}
+
+void Status::Destroy()
+{
+    delete Status::obj;
+    Status::obj = nullptr;
+}
+
+void Status::Init(const std::string& csvfile)
+{
+}
+
+void Status::Update()
+{
+}
+
+void Status::Save(const std::string& csvfile)
+{
+}
+
+float Status::GetBodyStaminaCurrent() const
+{
+    return m_bodyStaminaCurrent;
+}
+
+void Status::SetBodyStaminaCurrent(float mbodyStaminaCurrent)
+{
+    m_bodyStaminaCurrent = mbodyStaminaCurrent;
+}
+
+float Status::GetBodyStaminaMax() const
+{
+    return m_bodyStaminaMax;
+}
+
+void Status::SetBodyStaminaMax(float mbodyStaminaMax)
+{
+    m_bodyStaminaMax = mbodyStaminaMax;
+}
+
+float Status::GetBodyStaminaMaxSub() const
+{
+    return m_bodyStaminaMaxSub;
+}
+
+void Status::SetBodyStaminaMaxSub(float mbodyStaminaMaxSub)
+{
+    m_bodyStaminaMaxSub = mbodyStaminaMaxSub;
+}
+
+float Status::GetBrainStaminaCurrent() const
+{
+    return m_brainStaminaCurrent;
+}
+
+void Status::SetBrainStaminaCurrent(float mbrainStaminaCurrent)
+{
+    m_brainStaminaCurrent = mbrainStaminaCurrent;
+}
+
+float Status::GetBrainStaminaMax() const
+{
+    return m_brainStaminaMax;
+}
+
+void Status::SetBrainStaminaMax(float mbrainStaminaMax)
+{
+    m_brainStaminaMax = mbrainStaminaMax;
+}
+
+float Status::GetBrainStaminaMaxSub() const
+{
+    return m_brainStaminaMaxSub;
+}
+
+void Status::SetBrainStaminaMaxSub(float mbrainStaminaMaxSub)
+{
+    m_brainStaminaMaxSub = mbrainStaminaMaxSub;
+}
+
+float Status::GetExplosivePower() const
+{
+    return m_explosivePower;
+}
+
+void Status::SetExplosivePower(float mexplosivePower)
+{
+    m_explosivePower = mexplosivePower;
+}
+
+float Status::GetMuscleCurrent() const
+{
+    return m_muscleCurrent;
+}
+
+void Status::SetMuscleCurrent(float mmuscleCurrent)
+{
+    m_muscleCurrent = mmuscleCurrent;
+}
+
+float Status::GetMuscleMax() const
+{
+    return m_muscleMax;
+}
+
+void Status::SetMuscleMax(float mmuscleMax)
+{
+    m_muscleMax = mmuscleMax;
+}
+
+float Status::GetCarboCurrent() const
+{
+    return m_carboCurrent;
+}
+
+void Status::SetCarboCurrent(float mcarbo)
+{
+    m_carboCurrent = mcarbo;
+}
+
+float Status::GetCarboMax() const
+{
+    return m_carboMax;
+}
+
+void Status::SetCarboMax(float mcarbo)
+{
+    m_carboMax = mcarbo;
+}
+
+float Status::GetProteinCurrent() const
+{
+    return m_proteinCurrent;
+}
+
+void Status::SetProteinCurrent(float mprotein)
+{
+    m_proteinCurrent = mprotein;
+}
+
+float Status::GetProteinMax() const
+{
+    return m_proteinMax;
+}
+
+void Status::SetProteinMax(float mprotein)
+{
+    m_proteinMax = mprotein;
+}
+
+float Status::GetLipidCurrent() const
+{
+    return m_lipidCurrent;
+}
+
+void Status::SetLipidCurrent(float mlipid)
+{
+    m_lipidCurrent = mlipid;
+}
+
+float Status::GetLipidMax() const
+{
+    return m_lipidMax;
+}
+
+void Status::SetLipidMax(float mlipid)
+{
+    m_lipidMax = mlipid;
+}
+
+float Status::GetVitaminCurrent() const
+{
+    return m_vitaminCurrent;
+}
+
+void Status::SetVitaminCurrent(float mvitamin)
+{
+    m_vitaminCurrent = mvitamin;
+}
+
+float Status::GetVitaminMax() const
+{
+    return m_vitaminMax;
+}
+
+void Status::SetVitaminMax(float mvitamin)
+{
+    m_vitaminMax = mvitamin;
+}
+
+float Status::GetMineralCurrent() const
+{
+    return m_mineralCurrent;
+}
+
+void Status::SetMineralCurrent(float mmineral)
+{
+    m_mineralCurrent = mmineral;
+}
+
+float Status::GetMineralMax() const
+{
+    return m_mineralMax;
+}
+
+void Status::SetMineralMax(float mmineral)
+{
+    m_mineralMax = mmineral;
+}
+
+float Status::GetWaterCurrent() const
+{
+    return m_waterCurrent;
+}
+
+void Status::SetWaterCurrent(float mwater)
+{
+    m_waterCurrent = mwater;
+}
+
+float Status::GetWaterMax() const
+{
+    return m_waterMax;
+}
+
+void Status::SetWaterMax(float mwater)
+{
+    m_waterMax = mwater;
+}
+
+bool Status::GetFractureArm() const
+{
+    return m_fractureArm;
+}
+
+void Status::SetFractureArm(bool mfractureArm)
+{
+    m_fractureArm = mfractureArm;
+}
+
+bool Status::GetFractureLeg() const
+{
+    return m_fractureLeg;
+}
+
+void Status::SetFractureLeg(bool mfractureLeg)
+{
+    m_fractureLeg = mfractureLeg;
+}
+
+bool Status::GetHeadache() const
+{
+    return m_headache;
+}
+
+void Status::SetHeadache(bool mheadache)
+{
+    m_headache = mheadache;
+}
+
+bool Status::GetCold() const
+{
+    return m_cold;
+}
+
+void Status::SetCold(bool mcold)
+{
+    m_cold = mcold;
+}
+
+bool Status::GetStomachache() const
+{
+    return m_stomachache;
+}
+
+void Status::SetStomachache(bool mstomachache)
+{
+    m_stomachache = mstomachache;
+}
+
+bool Status::GetSleep() const
+{
+    return m_sleep;
+}
+
+void Status::SetSleep(bool msleep)
+{
+    m_sleep = msleep;
+}
+
+bool Status::GetDehydration() const
+{
+    return m_dehydration;
+}
+
+void Status::SetDehydration(bool mdehydration)
+{
+    m_dehydration = mdehydration;
+}
+
+bool Status::GetLackOfSleep() const
+{
+    return m_lackOfSleep;
+}
+
+void Status::SetLackOfSleep(bool mlackOfSleep)
+{
+    m_lackOfSleep = mlackOfSleep;
+}
+
