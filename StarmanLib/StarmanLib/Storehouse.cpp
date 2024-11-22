374e0264e50c44d9156eda2041861b4a4c3d339f
bibindon0814 <bibindon0814@gmail.com>
2024-11-22 15:53:28 +0900

    .

--- StarmanLib/StarmanLib/Storehouse.cpp
+++ StarmanLib/StarmanLib/Storehouse.cpp
@@ -1,1 +1,74 @@
 #include "Storehouse.h"
+#include "ItemManager.h"
+
+Storehouse* Storehouse::obj { nullptr };
+
+Storehouse* Storehouse::GetObj()
+{
+    if (obj == nullptr)
+    {
+        obj = new Storehouse();
+    }
+    return obj;
+}
+
+void Storehouse::Destroy()
+{
+    delete Storehouse::obj;
+    Storehouse::obj = nullptr;
+}
+
+void Storehouse::Init(const std::string& csvfile)
+{
+    std::vector<std::vector<std::string> > vss;
+    vss = csv::Read(csvfile);
+    for (std::size_t i = 1; i < vss.size(); ++i)
+    {
+        int workId = 0;
+        int workCount = 0;
+
+        workId = std::stoi(vss.at(i).at(0));
+        workCount = std::stoi(vss.at(i).at(1));
+        m_itemMap[workId] = workCount;
+    }
+}
+
+void Storehouse::Save(const std::string& csvfile)
+{
+    std::vector<std::vector<std::string>> vss;
+    std::vector<std::string> vs;
+    vs.push_back("ID");
+    vs.push_back("ŒÂ”");
+    vss.push_back(vs);
+    vs.clear();
+
+    for (auto it = m_itemMap.begin(); it != m_itemMap.end(); ++it)
+    {
+        vs.push_back(std::to_string(it->first));
+        vs.push_back(std::to_string(it->second));
+        vss.push_back(vs);
+        vs.clear();
+    }
+
+    csv::Write(csvfile, vss);
+}
+
+void Storehouse::AddItem(const int id)
+{
+    m_itemMap[id] = m_itemMap[id] + 1;
+}
+
+void Storehouse::RemoveItem(const int id)
+{
+    if (m_itemMap[id] >= 1)
+    {
+        m_itemMap[id] = m_itemMap[id] - 1;
+    }
+}
+
+int Storehouse::CountItem(const int id)
+{
+    return m_itemMap[id];
+}
+
+
--- StarmanLib/StarmanLib/Storehouse.h
+++ StarmanLib/StarmanLib/Storehouse.h
@@ -1,10 +1,40 @@
 #pragma once
 
+#include "HeaderOnlyCsv.hpp"
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
 // 倉庫クラス
 // 拠点に預けているアイテム
 // ストーリーの都合で拠点は途中で1度変わることに注意。
 // 拠点間で倉庫の中身を取り出せるようにはしない想定。
 class Storehouse
 {
+
+public:
+
+    static Storehouse* GetObj();
+
+    static void Destroy();
+
+    void Init(const std::string& csvfile);
+    void Save(const std::string& csvfile);
+
+    // 一個ずつしか増加減出来ない想定
+    void AddItem(const int id);
+    void RemoveItem(const int id);
+    int CountItem(const int id);
+
+private:
+
+    // シングルトンオブジェクト
+    static Storehouse* obj;
+
+    // 所持品リスト
+    // 同じアイテムを複数所持できることに注意
+    std::unordered_map<int, int> m_itemMap;
+    
 };
 
