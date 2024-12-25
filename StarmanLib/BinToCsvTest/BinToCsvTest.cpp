10ebaa01e8c85b1d9051b6b677ec350dbacabf5d
bibindon0814 <bibindon0814@gmail.com>
2024-12-25 20:11:54 +0900

    .

---
+++ StarmanLib/BinToCsvTest/BinToCsvTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace BinToCsvTest
+{
+    TEST_CLASS(BinToCsvTest)
+    {
+    public:
+        
+        TEST_METHOD(TestMethod1)
+        {
+        }
+    };
+}
---
+++ StarmanLib/CsvToBinTest/CsvToBinTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CsvToBinTest
+{
+    TEST_CLASS(CsvToBinTest)
+    {
+    public:
+        
+        TEST_METHOD(TestMethod1)
+        {
+        }
+    };
+}
