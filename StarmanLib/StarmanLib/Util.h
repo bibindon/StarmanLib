93ea76f851d6b5feb40946ef52ca378a30224fe2
bibindon0814 <bibindon0814@gmail.com>
2025-02-18 10:54:04 +0900

    .

--- StarmanLib/StarmanLib/Util.h
+++ StarmanLib/StarmanLib/Util.h
@@ -14,5 +14,17 @@ public:
                            const bool encrypt);
 
     static std::vector<std::string> split(const std::string& s, char delim = ':');
+
+    template <typename T>
+    static void EraseRemove(std::vector<T>& vec, const T& value)
+    {
+        vec.erase(std::remove(vec.begin(), vec.end(), value), vec.end());
+    }
+
+    template <typename T, typename Predicate>
+    static void EraseRemoveIf(std::vector<T>& vec, Predicate pred)
+    {
+        vec.erase(std::remove_if(vec.begin(), vec.end(), pred), vec.end());
+    }
 };
 
