5163495f699d697c2d0fa5a5b1c98aa43de784f6
bibindon0814 <bibindon0814@gmail.com>
2025-03-07 09:36:40 +0900

    .

---
+++ StarmanLib/StarmanLibTest/StorehouseTest.cpp
@@ -0,0 +1,36 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../StarmanLib/Inventory.h"
+#include <fstream>
+#include <sstream>
+#include <iterator>
+#include <string>
+#include "../StarmanLib/Storehouse.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace NSStarmanLib;
+
+namespace StarmanLibTest
+{
+
+TEST_CLASS(StorehouseTest)
+{
+public:
+
+    TEST_METHOD(TestMethod01)
+    {
+        auto storehouse = StorehouseManager::Get();
+
+        bool bException = false;
+        try
+        {
+            storehouse->Init("no-exist-file.csv");
+        }
+        catch (const std::exception&)
+        {
+            bException = true;
+        }
+        Assert::AreEqual(true, bException);
+    }
+};
+}
