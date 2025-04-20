8b0cfae428d88d5572f8a1daf75753b035c850e2
bibindon <bibindon0814@gmail.com>
2025-04-20 23:41:20 +0900

    .

---
+++ StarmanLib/StarmanLib/Help.cpp
@@ -0,0 +1,281 @@
+#include "Help.h"
+
+#include "PowereggDateTime.h"
+#include <time.h>
+#include "Util.h"
+
+using namespace NSStarmanLib;
+
+Help* Help::m_obj = nullptr;
+
+Help* NSStarmanLib::Help::Get()
+{
+    if (m_obj == nullptr)
+    {
+        m_obj = new Help();
+    }
+    return m_obj;
+}
+
+void NSStarmanLib::Help::Destroy()
+{
+    delete m_obj;
+    m_obj = nullptr;
+}
+
+void NSStarmanLib::Help::Init(const std::string& filepath)
+{
+    // 拾ってよいアイテムのリスト
+    // 3,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20,21,22,23,25,33,34,35,38,39,40,41,42,
+    // 54, 55, 56, 57
+    m_enableItemIdList.clear();
+    m_enableItemIdList.push_back(3);
+    m_enableItemIdList.push_back(5);
+    m_enableItemIdList.push_back(6);
+    m_enableItemIdList.push_back(7);
+    m_enableItemIdList.push_back(8);
+    m_enableItemIdList.push_back(9);
+    m_enableItemIdList.push_back(11);
+    m_enableItemIdList.push_back(12);
+    m_enableItemIdList.push_back(13);
+    m_enableItemIdList.push_back(14);
+    m_enableItemIdList.push_back(15);
+    m_enableItemIdList.push_back(16);
+    m_enableItemIdList.push_back(17);
+    m_enableItemIdList.push_back(18);
+    m_enableItemIdList.push_back(19);
+    m_enableItemIdList.push_back(20);
+    m_enableItemIdList.push_back(21);
+    m_enableItemIdList.push_back(22);
+    m_enableItemIdList.push_back(23);
+    m_enableItemIdList.push_back(25);
+    m_enableItemIdList.push_back(33);
+    m_enableItemIdList.push_back(34);
+    m_enableItemIdList.push_back(35);
+    m_enableItemIdList.push_back(38);
+    m_enableItemIdList.push_back(39);
+    m_enableItemIdList.push_back(40);
+    m_enableItemIdList.push_back(41);
+    m_enableItemIdList.push_back(42);
+    m_enableItemIdList.push_back(54);
+    m_enableItemIdList.push_back(55);
+    m_enableItemIdList.push_back(56);
+    m_enableItemIdList.push_back(57);
+
+    m_presentMap["サンカクマン"] = std::vector<ItemDef>();
+    m_presentMap["シカクマン"] = std::vector<ItemDef>();
+
+    std::vector<std::vector<std::string>> vvs = Util::ReadFromCsv(filepath, false);
+
+    auto itemManager = ItemManager::GetObj();
+    for (auto& helper : vvs)
+    {
+        auto name = helper.at(0);
+        for (auto& itemId : helper)
+        {
+            auto itemDef = itemManager->GetItemDef(itemId);
+            m_presentMap.at(name).push_back(itemDef);
+        }
+    }
+}
+
+void NSStarmanLib::Help::Update()
+{
+    auto datetime = PowereggDateTime::GetObj();
+
+    // 時が巻き戻っていたら回収アイテムをクリアする
+    bool backtime = BackTime();
+
+    if (backtime)
+    {
+        for (auto& x : m_presented)
+        {
+            x.second = false;
+        }
+
+        for (auto& x : m_presentMap)
+        {
+            x.second.clear();
+        }
+
+        m_previousYear = datetime->GetYear();
+        m_previousMonth = datetime->GetMonth();
+        m_previousDay = datetime->GetDay();
+        m_previousHour = datetime->GetHour();
+        return;
+    }
+
+    auto crossOver = CrossOver16();
+
+    if (crossOver)
+    {
+        for (auto& x : m_presentMap)
+        {
+            // アイテムをランダムに10個以下で選択
+            auto items = GetRandomItem();
+
+            // 受け取らずに翌日の16時になったら新しいアイテムになり、古いものは消失
+            x.second = items;
+        }
+    }
+
+    m_previousYear = datetime->GetYear();
+    m_previousMonth = datetime->GetMonth();
+    m_previousDay = datetime->GetDay();
+    m_previousHour = datetime->GetHour();
+}
+
+void NSStarmanLib::Help::Save(const std::string& filepath)
+{
+    // m_presentMapの内容を出力
+    std::vector<std::vector<std::string>> vvs;
+    std::vector<std::string> vs;
+    int work = 0;
+
+    vs.push_back("名前");
+    vs.push_back("アイテム1");
+    vs.push_back("アイテム2");
+    vs.push_back("アイテム3");
+    vs.push_back("アイテム4");
+    vs.push_back("アイテム5");
+    vs.push_back("アイテム6");
+    vs.push_back("アイテム7");
+    vs.push_back("アイテム8");
+    vs.push_back("アイテム9");
+    vs.push_back("アイテム10");
+    vvs.push_back(vs);
+    vs.clear();
+
+    for (auto& helper : m_presentMap)
+    {
+        vs.push_back(helper.first);
+        for (auto& item : helper.second)
+        {
+            auto id = item.GetId();
+            vs.push_back(std::to_string(id));
+        }
+        vvs.push_back(vs);
+        vs.clear();
+    }
+
+    Util::WriteToCsv(filepath, vvs, false);
+}
+
+std::vector<ItemDef> NSStarmanLib::Help::ReceiveItems(const std::string& npcName)
+{
+    auto present = m_presentMap.at(npcName);
+
+    m_presentMap.at(npcName).clear();
+
+    return present;
+}
+
+bool NSStarmanLib::Help::Received(const std::string& npcName)
+{
+    // 空だったら渡した後。
+    auto received = m_presentMap.at(npcName).empty();
+    return received;
+}
+
+std::vector<ItemDef> NSStarmanLib::Help::GetRandomItem()
+{
+    int rnd = 0;
+    int work = 0;
+    srand((unsigned int)time(NULL));
+
+    // 個数をランダムで決める(1~10)
+    int itemNum = 1;
+    {
+        rnd = rand();
+        work = rnd % 10 + 1;
+        itemNum = work;
+    }
+
+    // アイテムをランダムで決める
+    std::vector<ItemDef> randomItem;
+    {
+        auto idListNum = m_enableItemIdList.size();
+
+        for (int i = 0; i < itemNum; ++i)
+        {
+            rnd = rand();
+            work = rnd % idListNum;
+            int id = m_enableItemIdList.at(work);
+            auto itemDef = ItemManager::GetObj()->GetItemDef(id);
+            randomItem.push_back(itemDef);
+        }
+    }
+
+    return randomItem;
+}
+
+bool NSStarmanLib::Help::CrossOver16()
+{
+    auto datetime = PowereggDateTime::GetObj();
+    bool crossOver = false;
+
+    if (datetime->GetYear() > m_previousYear)
+    {
+        crossOver = true;
+    }
+    else if (datetime->GetYear() == m_previousYear)
+    {
+        if (datetime->GetMonth() > m_previousMonth)
+        {
+            crossOver = true;
+        }
+        else if (datetime->GetMonth() == m_previousMonth)
+        {
+            if (datetime->GetDay() > m_previousDay)
+            {
+                crossOver = true;
+            }
+            else if (datetime->GetDay() == m_previousDay)
+            {
+                if (datetime->GetHour() >= 16)
+                {
+                    if (m_previousHour < 16)
+                    {
+                        crossOver = true;
+                    }
+                }
+            }
+        }
+    }
+
+    return crossOver;
+}
+
+bool NSStarmanLib::Help::BackTime()
+{
+    auto datetime = PowereggDateTime::GetObj();
+    bool backtime = false;
+
+    if (datetime->GetYear() < m_previousYear)
+    {
+        backtime = true;
+    }
+    else if (datetime->GetYear() == m_previousYear)
+    {
+        if (datetime->GetMonth() < m_previousMonth)
+        {
+            backtime = true;
+        }
+        else if (datetime->GetMonth() == m_previousMonth)
+        {
+            if (datetime->GetDay() < m_previousDay)
+            {
+                backtime = true;
+            }
+            else if (datetime->GetDay() == m_previousDay)
+            {
+                if (datetime->GetHour() < m_previousHour)
+                {
+                    backtime = true;
+                }
+            }
+        }
+    }
+
+    return backtime;
+}
---
+++ StarmanLib/StarmanLib/Help.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <vector>
+#include <map>
+
+#include "ItemManager.h"
+
+//-------------------------------------------------
+// お手伝い機能
+//
+// 子供たちが拾ってきたアイテムを受け取れる。
+// 「話しかけたら受け取れる」であることに注意。
+// 依頼や指示はできない。
+// 児童労働法に触れるため。
+// クラフト中は受け取れない。
+// 16時以降に話しかけたら受け取れる。
+//-------------------------------------------------
+namespace NSStarmanLib
+{
+class Help
+{
+public:
+    static Help* Get();
+    static void Destroy();
+
+    void Init(const std::string& filepath);
+    void Update();
+    void Save(const std::string& filepath);
+
+    // 16時以降に話しかけたら受け取れる。
+    // 受け取ったら消失。
+    // 受け取らずに翌日の16時になったら新しいアイテムになり、古いものは消失
+    // 10個まで
+    std::vector<ItemDef> ReceiveItems(const std::string& npcName);
+
+    bool Received(const std::string& npcName);
+
+private:
+
+    static Help* m_obj;
+
+    std::map<std::string, std::vector<ItemDef>> m_presentMap;
+
+    // 16時を超えたら翌日の日付を保存し、現在の日付とする
+    int m_previousYear = 0;
+    int m_previousMonth = 0;
+    int m_previousDay = 0;
+    int m_previousHour = 0;
+
+    // アイテムを渡し済みか
+    std::map<std::string, bool> m_presented;
+
+    std::vector<ItemDef> GetRandomItem();
+
+    bool CrossOver16();
+
+    // 過去にさかのぼっている？
+    bool BackTime();
+
+    // 拾うことが可能なアイテムのIDのリスト
+    // 拾えるはずのないアイテムを拾えないようにするため。
+    std::vector<int> m_enableItemIdList;
+};
+}
+
